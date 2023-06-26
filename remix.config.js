d3cad5cc1d7062741645bab798662adea15e837d
Linh Le <linh.le01@your.rentals>
2023-06-26 14:13:33 +0700

    add prettier and update format

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  publicPath: "/_static/build/",
-  server: "./server.ts",
-  serverBuildPath: "server/index.js",
+  ignoredRouteFiles: ['**/.*'],
+  publicPath: '/_static/build/',
+  server: './server.ts',
+  serverBuildPath: 'server/index.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   future: {
